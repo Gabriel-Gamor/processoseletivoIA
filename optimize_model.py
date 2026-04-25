f8512478b1a03f1c7bcdd652ef5e6f9d6ee5cabd
Gabriel <byel@byel.arch>
2026-04-25 19:24:23 -0300

    envio teste dos codigos

--- optimize_model.py
+++ optimize_model.py
@@ -2,3 +2,29 @@ import tensorflow as tf
 import os
 
 #insira seu código aqui
+
+print("Carregando o modelo salvo (model.h5)...")
+model = tf.keras.models.load_model('model.h5')
+
+print("Configurando o conversor TensorFlow Lite...")
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+
+# Aplicando a técnica Dynamic Range Quantization
+print("Aplicando otimização: Dynamic Range Quantization...")
+converter.optimizations = [tf.lite.Optimize.DEFAULT]
+
+print("Convertendo o modelo...")
+tflite_quant_model = converter.convert()
+
+print("Salvando o modelo otimizado em model.tflite...")
+with open('model.tflite', 'wb') as f:
+    f.write(tflite_quant_model)
+
+print("Otimização concluída com sucesso!")
+
+# EVerificar a eficiência por meio do tamanho
+h5_size = os.path.getsize('model.h5') / 1024
+tflite_size = os.path.getsize('model.tflite') / 1024
+print(f"\n--- Comparativo de Tamanho ---")
+print(f"Modelo Original (.h5): {h5_size:.2f} KB")
+print(f"Modelo Otimizado (.tflite): {tflite_size:.2f} KB")
--- train_model.py
+++ train_model.py
@@ -3,3 +3,47 @@ from tensorflow import keras
 from tensorflow.keras import layers
 
 #insira seu código aqui
+import os
+import numpy as np
+
+print("Carregando o dataset MNIST:")
+
+# Vai tenta ler local  e caso não ache vai baixar
+if os.path.exists('mnist.npz'):
+    print("Arquivo mnist.npz encontrado localmente. Lendo offline:")
+    with np.load('mnist.npz', allow_pickle=True) as f:
+        x_train, y_train = f['x_train'], f['y_train']
+        x_test, y_test = f['x_test'], f['y_test']
+else:
+    print("Arquivo não encontrado localmente. Lendo Online:")
+    mnist = keras.datasets.mnist
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+# Normalizanodo os dados para o intervalo [0, 1]
+x_train = x_train.reshape(-1, 28, 28, 1).astype("float32") / 255.0
+x_test = x_test.reshape(-1, 28, 28, 1).astype("float32") / 255.0
+
+print("Construindo o modelo CNN:")
+model = keras.Sequential([
+    layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Flatten(),
+    layers.Dense(64, activation='relu'),
+    layers.Dense(10, activation='softmax')
+])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+print("Treinando o modelo (limite de 5 épocas para CI:)")
+model.fit(x_train, y_train, epochs=5, validation_data=(x_test, y_test), batch_size=64)
+
+loss, accuracy = model.evaluate(x_test, y_test, verbose=2)
+print(f"\nAcurácia final no conjunto de teste: {accuracy * 100:.2f}%")
+
+print("Salvando o modelo no formato Keras (model.h5):")
+model.save('model.h5')
+print("Treinamento concluído com sucesso!")
